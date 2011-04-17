66f8366776ca38c22c5741f52c0003c561ecdc44
iandunn <iandunn@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-17 04:06:18 +0000

    git-svn-id: https://plugins.svn.wordpress.org/overwrite-uploads/trunk@373896 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -3,7 +3,7 @@
 /**
  * Uninstaller. Removes options and settings from the database.
  * @package OverwriteUploads
- * @author Ian Dunn <ian.dunn@mpangodev.com>
+ * @author Ian Dunn <ian@iandunn.name>
  */
  
 if( defined('WP_UNINSTALL_PLUGIN') && WP_UNINSTALL_PLUGIN == 'overwrite-uploads/overwrite-uploads.php' )
