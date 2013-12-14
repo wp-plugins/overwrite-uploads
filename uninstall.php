0dbcafabf91445a24babda52ced502a6dc1899cb
iandunn <iandunn@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-14 21:28:38 +0000

    Moving closer to WordPress coding style
    git-svn-id: https://plugins.svn.wordpress.org/overwrite-uploads/trunk@822143 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -2,15 +2,14 @@
 
 /**
  * Uninstaller. Removes options and settings from the database.
- * @package OverwriteUploads
+ * @package overwriteUploads
  * @author Ian Dunn <ian@iandunn.name>
  */
  
-if( defined('WP_UNINSTALL_PLUGIN') && WP_UNINSTALL_PLUGIN == 'overwrite-uploads/overwrite-uploads.php' )
-{
+if( defined( 'WP_UNINSTALL_PLUGIN' ) && WP_UNINSTALL_PLUGIN == 'overwrite-uploads/overwrite-uploads.php' ) {
 	delete_option('ovup_options');
+} else {
+	die( 'Access denied.' );
 }
-else
-	die('Access denied.');
 
 ?>
